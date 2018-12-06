f079db3a97d9af71cd6f9b5d360d01129eacf96c
LastDective <xlgjsj004@sina.com>
2018-12-06 23:05:22 +0800

    提交测试文件

---
+++ src/test/java/com/leetcode/No1To100/code005LongestPalindromicSubstring/LongestPalindromicSubstringTest.java
@@ -0,0 +1,89 @@
+package com.leetcode.No1To100.code005LongestPalindromicSubstring;
+
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * LongestPalindromicSubstring Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>ʮ���� 6, 2018</pre>
+ */
+public class LongestPalindromicSubstringTest {
+
+
+    private LongestPalindromicSubstring lps = new LongestPalindromicSubstring();
+
+    /**
+     * Method: longestPalindrome(String s)
+     */
+    @Test
+    public void testLongestPalindrome() throws Exception {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: longestPalindromeDynamic(String s)
+     */
+    @Test
+    public void testLongestPalindromeDynamic() throws Exception {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: longestPalindromeExpandAroundCenter(String s)
+     */
+    @Test
+    public void testLongestPalindromeExpandAroundCenter() throws Exception {
+        String testString = "v12321p";
+        System.out.println(lps.longestPalindromeExpandAroundCenter(testString));
+    }
+
+    /**
+     * Method: testLongestPalindrome()
+     */
+    @Test
+    public void testTestLongestPalindrome() throws Exception {
+        System.out.println("sdfsdf");
+//TODO: Test goes here... 
+    }
+
+
+    /**
+     * Method: ifPalindromicString(String s)
+     */
+    @Test
+    public void testIfPalindromicString() throws Exception {
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = LongestPalindromicSubstring.getClass().getMethod("ifPalindromicString", String.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/
+    }
+
+    /**
+     * Method: expandAroundCenter(String s, int left, int right)
+     */
+    @Test
+    public void testExpandAroundCenter() throws Exception {
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = LongestPalindromicSubstring.getClass().getMethod("expandAroundCenter", String.class, int.class, int.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/
+    }
+
+} 
