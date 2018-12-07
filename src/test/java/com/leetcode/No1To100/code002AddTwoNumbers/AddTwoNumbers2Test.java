a71faeb395a9f65b8924f763a09bdc06ab418053
bruceliu <xlgjsj004@sina.com>
2018-12-07 14:54:31 +0800

    提交测试的代码

---
+++ src/test/java/com/leetcode/No1To100/code002AddTwoNumbers/AddTwoNumbers2Test.java
@@ -0,0 +1,49 @@
+package com.leetcode.No1To100.code002AddTwoNumbers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * AddTwoNumbers2 Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>十二月 7, 2018</pre>
+ */
+public class AddTwoNumbers2Test {
+
+    @BeforeEach
+    public void before() throws Exception {
+    }
+
+    @BeforeEach
+    public void after() throws Exception {
+    }
+
+    /**
+     * Method: addTwoNumbers(ListNode l1, ListNode l2)
+     */
+    @Test
+    public void testAddTwoNumbers() throws Exception {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: testAddTwoNumbers()
+     */
+    @Test
+    public void testTestAddTwoNumbers() throws Exception {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: generateSingleList(int[] arrs)
+     */
+    @Test
+    public void testGenerateSingleList() throws Exception {
+//TODO: Test goes here... 
+    }
+
+
+} 
---
+++ src/test/java/com/leetcode/No1To100/code003LongestSubstringWithoutRepeatingCharacters/LengthOfLongestSubstringTest.java
@@ -0,0 +1,25 @@
+package com.leetcode.No1To100.code003LongestSubstringWithoutRepeatingCharacters;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * LengthOfLongestSubstring Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>ʮ���� 7, 2018</pre>
+ */
+public class LengthOfLongestSubstringTest {
+
+    @BeforeEach
+    public void before() throws Exception {
+    }
+
+    @BeforeEach
+    public void after() throws Exception {
+    }
+
+
+} 
